f01cceed891a682ef3a25b7a7e062132d760d6c2
Ahmed Azzouz <n11450771@qut.edu.au>
2025-09-14 22:29:10 +1000

    testing action runner

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,6 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local
+  baseURL: 'http://3.25.197.207:5001', // live
   //baseURL: '/api', // live
   headers: { 'Content-Type': 'application/json' },
 });
