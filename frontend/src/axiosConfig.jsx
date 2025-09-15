0287629d52befcacba350543cd1970711614401a
Ahmed Azzouz <n11450771@qut.edu.au>
2025-09-15 21:42:34 +1000

    fixing issue with frontend in ci.yml

--- frontend/src/axiosConfig.jsx
+++ frontend/src/axiosConfig.jsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const axiosInstance = axios.create({
   baseURL: 'http://localhost:5001', // local
-  baseURL: 'http://3.106.215.165:5001', // live
+  baseURL: 'http:/3.106.215.165:5001', // live
   //baseURL: '/api', // live
   headers: { 'Content-Type': 'application/json' },
 });
