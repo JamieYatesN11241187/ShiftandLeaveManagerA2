94f6892102ce47a19db1e3bf17602872498421e8
Ahmed Azzouz <n11450771@qut.edu.au>
2025-09-29 22:34:02 +1000

    UserAuthTest.js file is added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -10,6 +10,13 @@ const generateToken = (id) => {
 
 const registerUser = async (req, res) => {
     const { name, email, password, role, dob } = req.body;
+    const user = new User({
+        name,
+        email,
+        password,
+        role,
+        dob
+    });
     try {
         const today = new Date();
         const birthDate = new Date(dob);
@@ -25,9 +32,8 @@ const registerUser = async (req, res) => {
 
         const userExists = await User.findOne({ email });
         if (userExists) return res.status(400).json({ message: 'User already exists' });
-
-        const user = await User.create({ name, email, password, role, dob });
-        res.status(201).json({ id: user.id, name: user.name, email: user.email, role: user.role, token: generateToken(user.id) });
+        await user.save();
+        res.status(201).json(user);
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
@@ -38,7 +44,7 @@ const loginUser = async (req, res) => {
     try {
         const user = await User.findOne({ email });
         if (user && (await bcrypt.compare(password, user.password))) {
-            res.json({ id: user.id, name: user.name, email: user.email, role:user.role, token: generateToken(user.id) });
+           res.status(200).json({ id: user.id, name: user.name, email: user.email, role:user.role, token: generateToken(user.id) });
         } else {
             res.status(401).json({ message: 'Invalid email or password' });
         }
